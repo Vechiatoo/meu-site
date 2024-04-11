c202b7c53f120b326a095564ed4dea20b8cf9355
Gabriel Vechiato de Souza <149793389+Vechiatoo@users.noreply.github.com>
2024-04-11 16:17:32 -0300

    calculadora

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const botoes = document.querySelectorAll('.botao');
 const caixas = document.querySelector('.caixa')
 // Obtém a div onde o resultado será exibido
 const resultadoDiv = document.querySelector('.resultado');
+// Obtém o elemento de áudio
+const som = document.getElementById('som');
 
 // Variável para armazenar a expressão
 let expressao = '';
@@ -14,6 +16,10 @@ botoes.forEach(botao => {
     if (botao.value !== undefined) {
       // Verifica se o botão clicado é o "="
       if (botao.classList.contains('recebe')) {
+        som.currentTime = 4;
+        // Reproduz o som apenas quando o botão "=" é clicado
+        som.play();
+
         // Remove os espaços em branco da expressão antes de calcular
         const expressaoSemEspacos = expressao.replace(/\s/g, '');
         console.log("Expressão sem espaços:", expressaoSemEspacos);
@@ -26,6 +32,13 @@ botoes.forEach(botao => {
         ajustarTamanhoFonte(resultadoDiv);
         // Limpa a expressão
         expressao = '';
+      } else if (botao.classList.contains('limpa')) { // Verifica se o botão clicado é o "C" (Clear)
+        // Limpa a expressão
+        expressao = '';
+        // Atualiza o conteúdo da div com a expressão limpa
+        resultadoDiv.textContent = expressao;
+        // Redefine o tamanho da fonte para o padrão
+        resultadoDiv.style.fontSize = "";
       } else {
         // Atualiza a expressão com o valor do botão clicado
         expressao += botao.value;
