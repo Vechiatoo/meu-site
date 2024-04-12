52cce53428aad9ddea8133ad11eb8edb94e28249
Gabriel Vechiato de Souza <149793389+Vechiatoo@users.noreply.github.com>
2024-04-12 09:24:20 -0300

    calculadora

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Obtém todos os botões com a classe "botao"
 const botoes = document.querySelectorAll('.botao');
+const botao = document.querySelectorAll('.botao');
 const caixas = document.querySelector('.caixa')
 // Obtém a div onde o resultado será exibido
 const resultadoDiv = document.querySelector('.resultado');
@@ -26,10 +27,9 @@ botoes.forEach(botao => {
         // Calcula o resultado da expressão
         const resultado = eval(expressaoSemEspacos);
         console.log("Resultado:", resultado);
-        // Substitui o conteúdo da div por uma imagem
-        resultadoDiv.innerHTML = '<img src="cc1785991065454291f79f04d3d66377b861e460_full.jpg" alt="Imagem" width="102%" height="81px" style="border-radius: 10px">';
-        // Ajusta o tamanho da fonte proporcionalmente ao tamanho da div
-        ajustarTamanhoFonte(resultadoDiv);
+        // Substitui o conteúdo da div por um numero aleatorio
+        resultadoDiv.innerHTML = Math.floor(Math.random() * 500)
+        botao.innerHTML = '<img src="cc1785991065454291f79f04d3d66377b861e460_full.jpg" alt="Imagem" width="102%" height="81px" style="border-radius: 10px">';
         // Limpa a expressão
         expressao = '';
       } else if (botao.classList.contains('limpa')) { // Verifica se o botão clicado é o "C" (Clear)
@@ -51,12 +51,4 @@ botoes.forEach(botao => {
   });
 });
 
-// Função para ajustar o tamanho da fonte proporcionalmente ao tamanho da div
-function ajustarTamanhoFonte(div) {
-  const maxWidth = div.offsetWidth;
-  const textWidth = div.scrollWidth;
-  const currentFontSize = parseInt(window.getComputedStyle(div).fontSize);
-  // Ajusta o tamanho da fonte proporcionalmente à largura do texto
-  const newFontSize = Math.floor((maxWidth / textWidth) * currentFontSize);
-  div.style.fontSize = newFontSize + "%";
-}
+
